2714c30016ad06a5e337f8ac6a98fbb63b6cbf81
ryanrwatkins <rwatkins@gwu.edu>
2024-06-25 16:03:44 -0400

    first commit

--- src/pw_file.py
+++
@@ -1,4 +0,0 @@
-# palm_api_key='AIzaSyBrXcGgFSm9-ZzuGoGrwrDGlFd8BrAayCw'
-palm_api_key = "AIzaSyCeeZ8jBtenFX2ymdnHQCOI3YMtfFjjx20"
-hf_api_key = "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"
-hf_api_key2 = "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"
