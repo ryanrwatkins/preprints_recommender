9e74c56f989562969f2f99262ce9d69db5c86e9d
ryanrwatkins <rwatkins@gwu.edu>
2024-06-25 16:47:47 -0400

    and other stuff

--- src/user_profile.py
+++ src/user_profile.py
@@ -1,2 +1,2 @@
-biography = "I am a researcher in education and AI"
+biography = "I am a researcher in education and AI and other stuff"
 adjacent_value = 2  # between 1 and 4  -- get from online version
